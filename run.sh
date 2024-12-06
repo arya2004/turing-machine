28661bb8b33bf20e89d5b7016f82bad82cb720e3
arya2004 <arya20j4@gmail.com>
2024-12-06 22:32:49 +0530

    Refactor Turing machine implementation: update output file name and enhance main logic with machine structure and execution functions

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-mkdir -p build && gcc src/main.c -o build/turing_machine && ./build/turing_machine
+mkdir -p build && gcc src/main.c -o build/turing_machine.out && ./build/turing_machine.out
--- src/main.c
+++ src/main.c
@@ -1,6 +1,97 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main(){
+typedef struct 
+{
+    bool *data;
+    size_t data_size;
+
+    size_t head;
+
+}Machine;
+
+typedef enum {
+    LEFT = -1,
+    STAY = 0,
+    RIGHT = 1,
+
+}Direction;
+
+
+// exprected value, value if true, direction if true,next if true, value if false, directoin if false, nxt if false
+typedef struct main
+{   
+    bool exprected;
+
+    bool write_yes;
+    Direction dir_yes;
+    size_t next_yes;
+
+    bool write_no;
+    Direction dir_no;
+    size_t next_no;
+
+}Instruction;
+
+void machine_randoize(Machine *machine)
+{   
+    srand(time(NULL));
+    for(size_t i = 0; i < machine->data_size; i++)
+    {
+        machine->data[i] = rand() % 2;
+    }
+
+}
+
+size_t machine_execute(Machine *machine, Instruction *instruction)
+{
+    if(machine->data[machine->head] == instruction->exprected)
+    {   
+        machine->data[machine->head] = instruction->write_yes;
+        machine->head += instruction->dir_yes;
+        return instruction->next_yes;
+    }
+    machine->data[machine->head] = instruction->write_no;
+    machine->head += instruction->dir_no;
+    return instruction->next_no;
+}
+
+void machine_print(Machine *machine)
+{
+    for(size_t i = 0; i < machine->data_size; i++)
+    {
+        printf("%d -> ", machine->data[i]);
+    }
+    printf("\n");
+}
+
+
+int main()
+{
     printf("Hellow\n");
+    Machine machine = {0};
+    machine.data_size = 3;
+    machine.data = malloc(sizeof(bool)*machine.data_size);
+    machine_randoize(&machine);
+
+    Instruction inst[] = {
+        {0, 1, RIGHT, 2, 0, RIGHT, 0},
+        {NULL},
+    };
+
+    size_t inst_count = sizeof(inst)/ sizeof(*inst);
+    size_t cur = 0;
+
+    machine_print(&machine);
+    while (cur <= inst_count - 1)
+    {   
+        cur = machine_execute(&machine, &inst[cur]);
+        machine_print(&machine);
+    }
+    
+
+    printf("\nWorld\n");
     return 0;
 }
