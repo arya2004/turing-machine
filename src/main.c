2de0aaa8eaa440e26cabe590953c3a71a3ce6d89
arya2004 <arya20j4@gmail.com>
2024-12-07 23:01:51 +0530

    Refactor machine execution logic: consolidate instruction handling into State struct and simplify main program flow

--- src/main.c
+++ src/main.c
@@ -20,20 +20,34 @@ typedef enum {
 }Direction;
 
 
+typedef struct 
+{
+    bool write;
+    Direction dir;
+    size_t next;
+    
+}State;
+
 // exprected value, value if true, direction if true,next if true, value if false, directoin if false, nxt if false
 typedef struct main
 {   
     bool exprected;
 
-    bool write_yes;
-    Direction dir_yes;
-    size_t next_yes;
+    State yes;
 
-    bool write_no;
-    Direction dir_no;
-    size_t next_no;
+    State no;
 
 }Instruction;
+
+typedef struct 
+{
+
+    Machine  *machine;
+    Instruction *instruction;
+    size_t curr;
+
+}Program;
+
 
 void machine_randoize(Machine *machine)
 {   
@@ -49,17 +63,18 @@ size_t machine_execute(Machine *machine, Instruction *instruction)
 {
     if(machine->data[machine->head] == instruction->exprected)
     {   
-        machine->data[machine->head] = instruction->write_yes;
-        machine->head += instruction->dir_yes;
-        return instruction->next_yes;
+        machine->data[machine->head] = instruction->yes.write;
+        machine->head += instruction->yes.dir;
+        return instruction->yes.next;
     }
-    machine->data[machine->head] = instruction->write_no;
-    machine->head += instruction->dir_no;
-    return instruction->next_no;
+    machine->data[machine->head] = instruction->no.write;
+    machine->head += instruction->no.dir;
+    return instruction->no.next;
 }
 
 void machine_print(Machine *machine)
-{
+{   
+    printf("head: %zu, ", machine->head);
     for(size_t i = 0; i < machine->data_size; i++)
     {
         printf("%d -> ", machine->data[i]);
@@ -71,24 +86,28 @@ void machine_print(Machine *machine)
 int main()
 {
     printf("Hellow\n");
+    Program program = {0};
     Machine machine = {0};
+   
     machine.data_size = 3;
     machine.data = malloc(sizeof(bool)*machine.data_size);
     machine_randoize(&machine);
+     program.machine = &machine;
 
     Instruction inst[] = {
-        {0, 1, RIGHT, 2, 0, RIGHT, 0},
+        {0, {1, RIGHT, 2},{ 0, RIGHT, 0}},
         {NULL},
     };
 
-    size_t inst_count = sizeof(inst)/ sizeof(*inst);
-    size_t cur = 0;
+    program.instruction = inst;
 
-    machine_print(&machine);
-    while (cur <= inst_count - 1)
+    size_t inst_count = sizeof(inst)/ sizeof(*inst);
+
+    machine_print(program.machine);
+    while (program.curr <= inst_count - 1)
     {   
-        cur = machine_execute(&machine, &inst[cur]);
-        machine_print(&machine);
+        program.curr = machine_execute(program.machine, &inst[program.curr]);
+        machine_print(program.machine);
     }
     
 
